4ba770a4608c3cd10ae219e34245465ccb9e3868
Renan Gomes Ovando <renangomesovando@MacBook-Pro-de-Renan-2.local>
2019-09-11 22:39:20 -0300

    pom and start configuration implemented

---
+++ robots-api/src/main/java/br/com/robots/Configuration.java
@@ -0,0 +1,12 @@
+package br.com.robots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Configuration {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Configuration.class, args);
+	}
+}
